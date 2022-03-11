16b843b9907f277a15e1d1dd58999e4c2a93855a
Phuong Ho <annawu0611@gmail.com>
2022-03-11 21:15:15 +0700

    veri noice

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ set -e
 
 # build
 # cd client
-npm run build
+npm ./client run build
 
 # navigate into the build output directory
-cd dist
+cd ./client/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
